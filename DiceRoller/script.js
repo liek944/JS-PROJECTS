15aca86579d3149373a214169409294b7e876f8f
liek944 <185925440+liek944@users.noreply.github.com>
2025-09-08 15:55:22 +0800

    Add new projects: Dice Roller, Palindrome Checker, Todo List, and Word Counter with complete functionality and styling

--- DiceRoller/script.js
+++ DiceRoller/script.js
@@ -7,7 +7,7 @@ let myDice = [
     "images/dice 6.png"
 ];
 
-// Add event listener for the button
+
 const btn = document.getElementById("roll-button");
 btn.addEventListener("click", rollDice);
 
@@ -15,18 +15,6 @@ function rollDice() {
     const dice = [1, 2, 3, 4, 5, 6];
     const selectDice = Math.floor(Math.random() * dice.length);
     const randomDice = dice[selectDice];
-    let diceImage = document.getElementById("dice-image");
-    if (randomDice === 1) {
-        diceImage.src = myDice[0];
-    } else if (randomDice === 2) {
-        diceImage.src = myDice[1];
-    } else if (randomDice === 3) {
-        diceImage.src = myDice[2];
-    } else if (randomDice === 4) {
-        diceImage.src = myDice[3];
-    } else if (randomDice === 5) {
-        diceImage.src = myDice[4];
-    } else {
-        diceImage.src = myDice[5];
-    }
+    const diceImage = document.getElementById("dice-image");
+    diceImage.src = myDice[randomDice - 1];
 }
